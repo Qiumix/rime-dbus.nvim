d693d2f21951523e2669d5b6bbb76bfc22a377ad
Qiumix <j487428862@gmail.com>
2026-02-14 18:54:16 +0800

    feat(core): impl basic functionality

--- lua/rime_dbus/core.lua
+++ lua/rime_dbus/core.lua
@@ -1,3 +1,69 @@
 local M = {}
-print("hello this is FRB")
+
+-- True for ascii mode
+local rime_last_state = true
+
+--- Get current rime's ascii state, asynchronously.
+--- @param callback fun(is_ascii: boolean)
+local function get_rime_state(callback)
+  local stdout = vim.uv.new_pipe(false)
+  local handle
+
+  handle = vim.uv.spawn("busctl", {
+    args = { "--user", "call", "org.fcitx.Fcitx5", "/rime", "org.fcitx.Fcitx.Rime1", "IsAsciiMode" },
+    stdio = { nil, stdout, nil }
+  }, function()
+    if stdout then stdout:close() end
+    if handle then handle:close() end
+  end)
+
+  if stdout then
+    stdout:read_start(function(err, data)
+      if err then return end
+      if data then
+        local is_ascii = data:find("true") ~= nil
+        vim.schedule(function()
+          callback(is_ascii)
+        end)
+      end
+      stdout:read_stop()
+    end)
+  end
+end
+
+--- Set current rime's ascii state, asynchronously.
+--- @param target_state boolean
+local function set_rime_state(target_state)
+  vim.uv.spawn("busctl", {
+    args = {
+      "--user", "call", "org.fcitx.Fcitx5", "/rime",
+      "org.fcitx.Fcitx.Rime1", "SetAsciiMode", "b",
+      target_state and "true" or "false"
+    },
+    detach = true
+  }, nil)
+end
+
+--- Leave Insert mode, memory current state
+--- and forcely set ascii mode
+function M.save_and_set_ascii()
+  get_rime_state(function(current_is_ascii)
+    rime_last_state = current_is_ascii
+    -- Lazy set
+    if not current_is_ascii then
+      set_rime_state(true)
+    end
+  end)
+end
+
+--- Enter Insert mode, set mode if memorized
+--- mode is not equal to current mode.
+function M.restore_state()
+  get_rime_state(function(current_is_ascii)
+    if current_is_ascii ~= rime_last_state then
+      set_rime_state(rime_last_state)
+    end
+  end)
+end
+
 return M
--- plugin/init.lua
+++ plugin/init.lua
@@ -1,4 +1,22 @@
-print("test for frd is executed!")
+-- Only with both linux and dbus
+if vim.fn.has("linux") == 0 or vim.fn.executable("busctl") == 0 then
+  return
+end
 
+-- Mark plugin loading
 if vim.g.loaded_rime_dbus then return end
 vim.g.loaded_rime_dbus = true
+
+local core = require("rime_dbus.core")
+
+local rime_group = vim.api.nvim_create_augroup("RimeAutoMode", { clear = true })
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  group = rime_group,
+  callback = core.save_and_set_ascii,
+})
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+  group = rime_group,
+  callback = core.restore_state,
+})
